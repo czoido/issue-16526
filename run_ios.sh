8577d8bb557794bc62ea2635c82eb109fa7d4f3e
czoido <mrgalleta@gmail.com>
2024-06-29 11:58:45 +0200

    wip

--- run_ios.sh
+++ run_ios.sh
@@ -3,6 +3,4 @@ conan install . -pr:h ios -s build_type=Debug -s arch=x86_64 --build=missing
 conan install . -pr:h ios -s build_type=Release -s arch=x86_64 --build=missing
 xcodegen generate
 xcodebuild -project app.xcodeproj -configuration Release -arch x86_64 -sdk iphonesimulator
-./build/Release/app
 xcodebuild -project app.xcodeproj -configuration Debug -arch x86_64 -sdk iphonesimulator
-./build/Debug/app
